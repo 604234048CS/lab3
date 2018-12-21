0a815883554607191ae4a693e711f8afba6e1876
604234048CS <604234048@parichat.skru.ac.th>
2018-12-21 16:44:21 +0700

    fadel32

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { AboutPage } from './../about/about';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -8,6 +9,11 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+
+  }
+
+gotoPage(){
+  this.navCtrl.push(AboutPage);
 
   }
 
